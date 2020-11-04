8047e8fc20be038e46bf68d7e56646679f80e40c
hungpham1530 <hgn.pham@gmail.com>
2020-11-03 16:29:22 -0800

    work on json class

---
+++ CISC 191 Final Project/src/json.java
@@ -0,0 +1,19 @@
+public class Json {
+
+    public static void writeJsonSimpleDemo(String filename) throws Exception {
+        JSONObject sampleObject = new JSONObject();
+        sampleObject.put("name", "Stackabuser");
+        sampleObject.put("age", 35);
+
+        JSONArray messages = new JSONArray();
+        messages.add("Hey!");
+        messages.add("What's up?!");
+
+        sampleObject.put("messages", messages);
+        Files.write(Paths.get(filename), sampleObject.toJSONString().getBytes());
+    }
+
+    public static void main(String[] args) throws Exception {
+        writeJsonSimpleDemo("json/example.json");
+    }
+}
